4f7de0ceef0fe90586d1f4a88bfa92b472672df1
Patrick Salomon <patrick.salomon@jomediainc.com>
2015-12-09 09:11:29 -0500

    add alerts type in view

--- index.js
+++ index.js
@@ -241,8 +241,8 @@ if (args['--dev']) {
 		var freq = (Math.floor((Math.random() * 10) + 1) * 1000000) + 10000000;
 		var body = JSON.stringify({frequency: freq, threshold: Math.floor(Math.random() * 10), alert: []});
 		// freq = 5000;
-		// var body = JSON.stringify({frequency: freq, threshold: Math.floor(Math.random() * 10), alert: [{'type': 'email', 'data': {'email': 'no-reply@infra.systems'}}]});
 		// var body = JSON.stringify({frequency: freq, threshold: Math.floor(Math.random() * 10), alert: [{'type': 'email', 'data': {'email': 'patrick.salomon@jomediainc.com'}}]});
+		// var body = JSON.stringify({frequency: freq, threshold: Math.floor(Math.random() * 10), alert: [{'type': 'email', 'data': {'email': 'patrick.salomon@jomediainc.com'}}, {'type': 'hipchat'}, {'type': 'toto'}]});
 		http.request({
 			host: 'localhost', port: config.port, method: 'POST',
 			path: '/report/test-' + Math.floor(Math.random() * 10),
