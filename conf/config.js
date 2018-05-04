1b7290805cda2e37df69d05c5c5928d9cbd79c67
Tomasz Rakowski <akayami@gmail.com>
2018-05-04 16:08:30 -0400

    Phase 2

--- conf/config.js
+++ conf/config.js
@@ -1,39 +1,39 @@
 const config = {};
 
-config._dep = {
-	winston: require('winston')
-};
+// config._dep = {
+// 	winston: require('winston')
+// };
 
 config.port = 8081;
 
-config.email = {
-	from: 'no-reply-crontol-freak@infra.systems',
-	smtpconf: null
-};
+// config.email = {
+// 	from: 'no-reply-crontol-freak@infra.systems',
+// 	smtpconf: null
+// };
 
-config.winston = {
-	transports: [
-		new(config._dep.winston.transports.Console)({
-			timestamp: true,
-			colorize: true
-		})
-	]
-};
+// config.winston = {
+// 	transports: [
+// 		new(config._dep.winston.transports.Console)({
+// 			timestamp: true,
+// 			colorize: true
+// 		})
+// 	]
+// };
 
-config.defaultAlert = [{
-	type: 'custom',
-	backend: new(config._dep.winston.Logger)({
-		transports: [
-			new(config._dep.winston.transports.File)({
-				filename: '/tmp/crontol-freak.log',
-				timestamp: true,
-				colorize: true
-			})
-		]
-	}),
-	notify: function(msg) {
-		this.backend.error(msg);
-	}
-}];
+// config.defaultAlert = [{
+// 	type: 'custom',
+// 	backend: new(config._dep.winston.Logger)({
+// 		transports: [
+// 			new(config._dep.winston.transports.File)({
+// 				filename: '/tmp/crontol-freak.log',
+// 				timestamp: true,
+// 				colorize: true
+// 			})
+// 		]
+// 	}),
+// 	notify: function(msg) {
+// 		this.backend.error(msg);
+// 	}
+// }];
 
 module.exports = config;
